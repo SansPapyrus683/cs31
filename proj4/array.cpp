2bc67443892377379e84facee070616237d26f5a
SansPapyrus683 <55369003+SansPapyrus683@users.noreply.github.com>
2023-11-03 16:01:03 -0700

    Update array.cpp

--- proj4/array.cpp
+++ proj4/array.cpp
@@ -69,6 +69,9 @@ int enumerateRuns(const string a[], int n) {
     if (n < 0) {
         return -1;
     }
+    if (n == 0) {
+        return 0;
+    }
     int runs = 1;
     for (int i = 1; i < n; i++) {
         if (a[i] != a[i - 1]) {
