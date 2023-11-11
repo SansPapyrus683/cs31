ac4bcc4261bfe25febfa72c33dfa3a34e54a92cb
Kevin Sheng <kevinsheng2005@gmail.com>
2023-11-11 13:02:57 -0800

    comments & some report stuff

--- proj5/render.cpp
+++ proj5/render.cpp
@@ -11,6 +11,7 @@ bool isPunctuation(char c) {
     return strchr(".?!:", c) != NULL;
 }
 
+/** @return the number of spaces needed after a word */
 int spacesAfter(char str[]) {
     int len = strlen(str);
     return len == 0 ? 0 : (isPunctuation(str[len - 1]) ? 2 : 1);
@@ -25,17 +26,19 @@ int render(int linLen, istream& inf, ostream& outf) {
         return 2;
     }
 
-    char lastPrinted[MAX_WORD_LEN + 1];
+    char lastPrinted[MAX_WORD_LEN + 1];  // +1 for null terminator
     lastPrinted[0] = '\0';
     int currLen = 0;
 
     int retCode = 0;
-    bool lastWasPara = true;
-    bool anything = false;
-    char word[MAX_WORD_LEN + 1];  // +1 for null terminator
+    bool lastWasPara = true;  // was the last thing we outputted a paragraph?
+    bool anything = false;  // did we even read in any words
+    char word[MAX_WORD_LEN + 1];
     while (inf >> word) {
+        // first check if we should handle this as a paragraph
         if (strcmp(word, "@P@") == 0) {
             if (!lastWasPara) {
+                // reset & print only if the last token wasn't a paragraph
                 outf << "\n\n";
                 lastPrinted[0] = '\0';
                 currLen = 0;
@@ -44,26 +47,33 @@ int render(int linLen, istream& inf, ostream& outf) {
             continue;
         }
 
-        int len = strlen(word);
-        int prev = 0;
+        int len = strlen(word);  // shorthand
+        int prev = 0;  // the starting index of the current word portion
         for (int i = 0; i < len; i++) {
+            // end a word portion at a hyphen or at the end of the word
             if (word[i] == '-' || i == len - 1) {
-                int pLen = i - prev + 1;
+                int pLen = i - prev + 1;  // the length of the word part
                 
                 char slice[MAX_WORD_LEN + 1];
                 strncpy(slice, word + prev, pLen);
-                slice[pLen] = '\0';
+                slice[pLen] = '\0';  // c sucks
                 
                 int spaces = spacesAfter(lastPrinted);
+                // the current word along with the previous spaces have to fit
                 if (currLen + spaces + pLen > linLen) {
+                    // output linLen length chunks of the string until the end
                     for (int at = 0; at < pLen; at += linLen) {
                         outf << '\n';
                         for (int j = at; j < min(pLen, at + linLen); j++) {
                             outf << slice[j];
                         }
                     }
+                    if (pLen > linLen) {
+                        retCode = 1;
+                    }
                     currLen = len % linLen;
                 } else {
+                    // if it fits, we can just output the spaces & the word
                     for (int i = 0; i < spaces; i++) {
                         outf << ' ';
                     }
@@ -71,11 +81,12 @@ int render(int linLen, istream& inf, ostream& outf) {
                     currLen += spaces + len;
                 }
 
-                strcpy(lastPrinted, slice);
+                strcpy(lastPrinted, slice);  // update the last word printed
 
-                prev = i + 1;
+                prev = i + 1;  // and the start of the next word
             }
         }
+
         anything = true;
         lastWasPara = false;
     }
